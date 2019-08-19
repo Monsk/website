791db22dc3d34c2e16b939ee0b3e735651fbe61a
simon@hingehealth.com <simonhunter28@gmail.com>
2019-08-18 20:26:22 -0700

    Fix 009 migration

--- db_repository/versions/009_migration.py
+++ db_repository/versions/009_migration.py
@@ -3,26 +3,20 @@ from migrate import *
 
 
 from migrate.changeset import schema
-pre_meta = MetaData()
-post_meta = MetaData()
-user = Table('user', post_meta,
-    Column('id', String(length=64), primary_key=True, nullable=False),
-    Column('name', String(length=64)),
-    Column('email', String(length=64)),
-    Column('profile_pic', String(length=120)),
-)
 
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    pre_meta.bind = migrate_engine
-    post_meta.bind = migrate_engine
-    post_meta.tables['user'].create()
+    meta = MetaData(bind=migrate_engine)
+    users = Table('users', meta, autoload=True)
+    users.c.user_id.alter(type=String(64))
+    users.c.profile_pic.alter(type=String(120))
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    pre_meta.bind = migrate_engine
-    post_meta.bind = migrate_engine
-    post_meta.tables['user'].drop()
+    meta = MetaData(bind=migrate_engine)
+    users = Table('users', meta, autoload=True)
+    users.c.user_id.alter(type=Integer)
+    users.c.profile_pic.alter(type=Text)
