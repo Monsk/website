85377508b5ec2f50f3b4301ee54138a5e7c61a07
simon@hingehealth.com <simonhunter28@gmail.com>
2019-08-18 20:28:32 -0700

    Fix 009 migration

--- db_repository/versions/009_migration.py
+++ db_repository/versions/009_migration.py
@@ -9,14 +9,14 @@ def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     meta = MetaData(bind=migrate_engine)
-    users = Table('users', meta, autoload=True)
-    users.c.user_id.alter(type=String(64))
-    users.c.profile_pic.alter(type=String(120))
+    user = Table('user', meta, autoload=True)
+    user.c.user_id.alter(type=String(64))
+    user.c.profile_pic.alter(type=String(120))
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     meta = MetaData(bind=migrate_engine)
-    users = Table('users', meta, autoload=True)
-    users.c.user_id.alter(type=Integer)
-    users.c.profile_pic.alter(type=Text)
+    user = Table('user', meta, autoload=True)
+    user.c.user_id.alter(type=Integer)
+    user.c.profile_pic.alter(type=Text)
